90b573e93460e5449dafd0c0946c12deed2bce75
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-01 16:21:43 -0400

    token updates

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'blogful-client-auth-token',
+  TOKEN_KEY: ':b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&=',
+  API_TOKEN: ':b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&='
 }
