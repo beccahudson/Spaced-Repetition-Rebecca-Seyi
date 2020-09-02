e8b6ba414765ceb69db3de633b5c0219b727579d
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-02 13:05:58 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://sleepy-savannah-71620.herokuapp.com/api',
   TOKEN_KEY: ':b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&=',
   API_TOKEN: ':b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&='
 }
