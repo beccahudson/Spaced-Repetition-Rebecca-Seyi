f056773fa68ad43231e922449f2239ee9b721c76
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-02 14:19:43 -0400

    changed heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://sleepy-savannah-71620.herokuapp.com/api',
+  API_ENDPOINT: 'https://young-caverns-95905.herokuapp.com/api',
   TOKEN_KEY: ':b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&=',
   API_TOKEN: ':b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&='
 }
